1b648dca40536b0ab043debe07f9b7fb8b2b3f8a
Samuel Rodrigues <sam200133@gmail.com>
2022-01-26 14:19:24 +0100

    added CPU infos

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,9 @@
-use rusty_hass_laptop::{hass, sensors::battery::BatteryProvider, sensors::Provider, Config};
+use rusty_hass_laptop::{
+    hass,
+    sensors::Provider,
+    sensors::{battery::BatteryProvider, cpu::CpuProvider},
+    Config,
+};
 
 fn main() -> Result<(), battery::Error> {
     let mut client = hass::Client::new(Config::new()).unwrap();
@@ -9,12 +14,18 @@ fn main() -> Result<(), battery::Error> {
     client.config.save_to_file().unwrap();
 
     let mut battery = BatteryProvider::new();
+    let mut cpu = CpuProvider::new();
     battery.update_all().unwrap();
+    cpu.update_all().unwrap();
     client.register_sensor(&battery.level).unwrap();
+    client.register_sensor(&cpu.cpu_freq).unwrap();
+    client.register_sensor(&cpu.cpu_cores).unwrap();
     loop {
         battery.update_all().unwrap();
         println!("{:?}", battery.level.state);
         client.update_sensor(&battery.level).unwrap();
+        client.update_sensor(&cpu.cpu_freq).unwrap();
+        client.update_sensor(&cpu.cpu_cores).unwrap();
         std::thread::sleep(std::time::Duration::from_secs(60));
     }
 }
--- src/sensors/cpu.rs
+++ src/sensors/cpu.rs
@@ -1,26 +1,25 @@
 use super::{Provider, Sensor, SensorData, SensorRegisterData, SensorType, SensorUpdateData};
-use sys_info;
 use serde::Serialize;
-
+use sys_info;
 
 pub struct CpuProvider {
-pub cpu_num:u32,
-pub cpu_freq : u64,
+    pub cpu_cores: CpuCores,
+    pub cpu_freq: CpuFreq,
 }
 
 impl Provider for CpuProvider {
     /// Init the CPU provider
     fn new() -> Self {
         CpuProvider {
-            cpu_num: sys_info::cpu_num().unwrap(),
-            cpu_freq: sys_info::cpu_speed().unwrap(),
+            cpu_cores: CpuCores::new(),
+            cpu_freq: CpuFreq::new(),
         }
     }
 
     /// Update the CPU provider
     fn update_all(&mut self) -> Result<(), &'static str> {
-        self.cpu_num = sys_info::cpu_num().unwrap();
-        self.cpu_freq = sys_info::cpu_speed().unwrap();
+        self.cpu_cores.state = CpuCores::get_current(self).unwrap();
+        self.cpu_freq.state = CpuFreq::get_current(self).unwrap();
         Ok(())
     }
 }
@@ -28,13 +27,17 @@ impl Provider for CpuProvider {
 #[derive(Serialize, Debug)]
 pub struct CpuFreq {
     pub state: u64,
+    unique_id: String,
 }
 
 impl Sensor<CpuProvider> for CpuFreq {
     type StateType = u64;
 
     fn new() -> Self {
-        CpuFreq {state : 0}
+        CpuFreq {
+            state: 0,
+            unique_id: String::from("cpu_freq"),
+        }
     }
 
     #[allow(unused)]
@@ -46,12 +49,12 @@ impl Sensor<CpuProvider> for CpuFreq {
         SensorData::RegisterSensor::<Self::StateType> {
             data: SensorRegisterData {
                 r#type: SensorType::Sensor,
-                unique_id: String::from("battery_level"),
-                name: String::from("Battery Level"),
+                unique_id: self.unique_id.clone(),
+                name: String::from("CPU Frequency"),
                 state: self.state,
-                device_class: None,
-                icon: Some(String::from("mdi:battery-unknown")),
-                unit_of_measurement: Some(String::from("%")),
+                device_class: Some(String::from("frequency")),
+                icon: Some(String::from("mdi:sine-wave")),
+                unit_of_measurement: Some(String::from("MHz")),
                 state_class: None,
                 entity_category: None,
             },
@@ -62,11 +65,59 @@ impl Sensor<CpuProvider> for CpuFreq {
         SensorData::UpdateSensorStates::<Self::StateType> {
             data: SensorUpdateData {
                 r#type: SensorType::Sensor,
-                unique_id: String::from("cpu_freq"),
+                unique_id: self.unique_id.clone(),
                 state: self.state,
                 icon: Some(String::from("mdi:sine-wave")),
-            }
+            },
+        }
+    }
+}
+
+#[derive(Serialize, Debug)]
+pub struct CpuCores {
+    pub state: u32,
+    unique_id: String,
+}
+
+impl Sensor<CpuProvider> for CpuCores {
+    type StateType = u32;
+
+    fn new() -> Self {
+        CpuCores {
+            state: 0,
+            unique_id: String::from("cpu_cores"),
         }
     }
 
+    #[allow(unused)]
+    fn get_current(provider: &CpuProvider) -> Result<Self::StateType, &'static str> {
+        Ok(sys_info::cpu_num().unwrap())
+    }
+
+    fn get_register_info(&self) -> SensorData<Self::StateType> {
+        SensorData::RegisterSensor::<Self::StateType> {
+            data: SensorRegisterData {
+                r#type: SensorType::Sensor,
+                unique_id: self.unique_id.clone(),
+                name: String::from("CPU Cores"),
+                state: self.state,
+                device_class: None,
+                icon: Some(String::from("mdi:memory")),
+                unit_of_measurement: None,
+                state_class: None,
+                entity_category: None,
+            },
+        }
+    }
+
+    fn get_update_info(&self) -> SensorData<Self::StateType> {
+        SensorData::UpdateSensorStates::<Self::StateType> {
+            data: SensorUpdateData {
+                r#type: SensorType::Sensor,
+                unique_id: self.unique_id.clone(),
+                state: self.state,
+                icon: Some(String::from("mdi:memory")),
+            },
+        }
+    }
 }
